090c4681ecd5c5b5f18db7c40fa971f371cce1af
mustafa-raza-26 <mustafa778823@gmail.com>
2025-09-21 00:10:16 +0500

    Google Classroom

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function show() {
     document.getElementById("forms").style.display = "block";
-}
+};
 
 let display = document.getElementById("show")
 let list = document.getElementById("list")
@@ -10,6 +10,17 @@ let subject = document.getElementById("subject")
 let teacher = document.getElementById("teacher")
 
 function create() {
+
+    let cardData = {
+        className: class_name.value,
+        section: section.value,
+        subject: subject.value,
+        teacher: teacher.value
+    };
+
+    let storedData = JSON.parse(localStorage.getItem("cards")) || [];
+    storedData.push(cardData);
+    localStorage.setItem("cards", JSON.stringify(storedData));
 
     display.innerHTML +=`
             <div class="box">
@@ -28,20 +39,49 @@ function create() {
             </footer>
         </div>`
 
-        document.getElementById("forms").style.display = "none";
+    document.getElementById("forms").style.display = "none";
 
-        list.innerHTML += `
+    list.innerHTML += `
         <div class="feature">
         <h5>${class_name.value}</h5>
         <h6>${section.value}</h6>
         </div>`
 
-        class_name.value = "";
-        section.value = "";
-        subject.value = "";
-        teacher.value = "";
-}
+    class_name.value = "";
+    section.value = "";
+    subject.value = "";
+    teacher.value = "";
+};
+
+window.onload = function () {
+    let storedData = JSON.parse(localStorage.getItem("cards")) || [];
+
+    storedData.forEach(cardData => {
+        display.innerHTML +=`
+            <div class="box">
+            <img src="img/cardd" alt="" class="bgg">
+            <div class="text">
+            <h2>${cardData.className}</h2>
+            <h5>${cardData.section}</h5>
+            <h5>${cardData.subject}</h5>
+            <h5>${cardData.teacher}</h5>
+            </div>
+            <footer>
+            <hr>
+            <span><img src="img/group.svg" alt="" class="bgg"></span>
+            <span><img src="img/calender.svg" alt="" class="bgg"></span>
+            <span><img src="img/dot.svg" alt="" class="bgg"></span>
+            </footer>
+        </div>`;
+
+        list.innerHTML += `
+        <div class="feature">
+        <h5>${cardData.className}</h5>
+        <h6>${cardData.section}</h6>
+        </div>`;
+    });
+};
 
 function cancel() {
     document.getElementById("forms").style.display = "none";
-}   
+};
